c13f5282fdc229581e2f9b8bae518a30a3e3df0d
albdom22 <albertodl22@gmail.com>
2025-02-18 18:01:15 +0100

    userlist y service2

---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/services/user.service';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+
+  users: any[] = [];
+  newName: string = '';
+  newEmail: string = '';
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(){
+    this.users = this.userService.getUsers();
+  }
+
+  addUser() {
+    if (this.newName.trim() && this.newEmail.trim()) {
+      this.userService.addUser(this.newName, this.newEmail);
+      this.newName = '';
+      this.newEmail = '';
+    }
+  }
+
+  deleteUser(id: number) {
+    this.userService.deleteUser(id);
+    this.users = this.userService.getUsers();
+  }
+
+  
+  editUser(id: number) {
+    const user = this.users.find(u => u.id === id);
+    if (user) {
+      const newName = prompt('Nuevo nombre:', user.name);
+      const newEmail = prompt('Nuevo email:', user.email);
+
+      if (newName !== null && newEmail !== null) {
+        this.userService.updateUser(id, newName, newEmail);
+      }
+    }
+  }
+  
+}
---
+++ src/services/user.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private users = [
+    {   id: 1, name: 'David', email: 'david@example.com'},
+    {   id: 2, name: 'Carlos', email: 'carlos@example.com'},
+    {   id: 3, name: 'Maria', email: 'maria@example.com'},
+    {   id: 4, name: 'Ismael', email: 'ismael@example.com'},
+    {   id: 5, name: 'Hugo', email: 'hugo@example.com'},
+    {   id: 6, name: 'Paloma', email: 'paloma@example.com'},
+    {   id: 7, name: 'Miriam', email: 'miriam@example.com'},
+    {   id: 8, name: 'Pablo', email: 'pablo@example.com'}
+  ];
+
+  constructor() { }
+
+  /**
+   * Funcion para obtener la lista de usuarios
+   * @returns lista de usuarios
+   */
+  getUsers(){
+    return this.users;
+  }
+
+
+  /**
+   * Funcion para agregar un usuario a la lista
+   */
+  addUser(name: string, email:string){
+    const newUser = {id : this.users.length + 1, name, email};
+    this.users.push(newUser);
+  }
+
+  /**
+   * Funcion para eliminar un usuario
+   *  */
+  deleteUser(userId: number){
+    this.users = this.users.filter(user => user.id !== userId);
+  }
+
+  /**
+   * Funcion para actualizar un usuario
+   * @param userId 
+   * @param name 
+   * @param email 
+   */
+  updateUser(userId: number, name: string, email: string){
+    const user = this.users.find(user => user.id === userId);
+    if(user){
+      user.name = name;
+      user.email = email;
+  
+    }
+  }
+
+
+}
